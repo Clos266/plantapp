8246ece6297002c1c960566981da51cc5035ae7c
Clos266 <clos266@gmail.com>
2025-10-07 18:09:58 +0200

    refactor: comment out AuthProvider import for testing purposes

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
-import { AuthProvider } from "./contexts/AuthProvider";
+// import { AuthProvider } from "./contexts/AuthProvider";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
